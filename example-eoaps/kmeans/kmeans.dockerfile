bc038f558405937810a58acc908901900fc1c504
Florian Katerndahl <florian@katerndahl.com>
2026-08-25 11:18:41 +0200

    update kmeans workflow

--- example-eoaps/kmeans/kmeans.dockerfile
+++ example-eoaps/kmeans/kmeans.dockerfile
@@ -4,7 +4,7 @@ ENV DEBIANFRONTEND=noninteractive
 
 RUN apt update && \
     apt install -y --no-install-suggests \
-        r-base r-base-core r-base-dev file r-cran-jsonlite r-cran-purrr libproj-dev libproj25 libudunits2-dev libgeos-dev libsqlite3-dev jq python3-pystac libuv1-dev libuv1t64 && \
+        r-base r-base-core r-base-dev file r-cran-jsonlite r-cran-purrr libproj-dev libproj25 libudunits2-dev libgeos-dev libsqlite3-dev jq python3-pystac python3-rasterio python3-shapely python3-fiona libuv1-dev libuv1t64 && \
      rm -rf /var/lib/apt/lists/*
 
 RUN Rscript -e "install.packages(c('terra', 'raster', 'tmap', 'rstac'))"
--- example-eoaps/kmeans/scripts/output-generator.py
+++ example-eoaps/kmeans/scripts/output-generator.py
@@ -1,6 +1,7 @@
 #  Copyright (c) 2026- by the Eozilla team and contributors
 #  Permissions are hereby granted under the terms of the Apache 2.0 License:
 #  https://opensource.org/license/apache-2-0.
+import json
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from datetime import datetime
@@ -8,8 +9,12 @@ from pathlib import Path
 from sys import exit
 from typing import Dict, List
 
+import fiona
 import pystac
+import rasterio as rio
+import shapely
 
+from fiona.transform import transform_geom
 
 @dataclass
 class Arguments:
@@ -41,10 +46,29 @@ if __name__ == "__main__":
     )
 
     for input_file in parsed_args.inputs:
+        with rio.open(input_file) as dataset:
+            bbox = dataset.bounds
+            width = dataset.width
+            height = dataset.height
+            transformation = dataset.transform
+            footprint = shapely.Polygon(
+                [
+                    transformation * (0, 0),
+                    transformation * (width, 0),
+                    transformation * (width, height),
+                    transformation * (0, height),
+                ]
+            )
+            src_epsg = dataset.crs.to_epsg()
+
+        item_geometry = transform_geom(
+                src_epsg, "EPSG:4326", json.loads(shapely.to_geojson(footprint))
+            )
+
         item = pystac.item.Item(
             id=input_file.stem,
-            geometry=None,
-            bbox=None,
+            geometry=dict(item_geometry),
+            bbox=fiona.bounds(item_geometry),
             datetime=datetime.now(),
             properties={},
             assets={input_file.stem: pystac.asset.Asset(href=input_file)},
